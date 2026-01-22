eb0cfaa43a293ed732c03b275976d190e39b8544
DiscipleDeveloper <ntumale@byupathway.edu>
2026-01-23 03:26:51 +0800

    Learned instances

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fractionOne = new Fraction();
+        Console.WriteLine(fractionOne.GetFractionString());
+        Console.WriteLine(fractionOne.GetDecimalValue());
+
+        Fraction fractionTwo = new Fraction(5);
+        Console.WriteLine(fractionTwo.GetFractionString());
+        Console.WriteLine(fractionTwo.GetDecimalValue());
+
+        Fraction fractionThree = new Fraction(3, 4);
+        Console.WriteLine(fractionThree.GetFractionString());
+        Console.WriteLine(fractionThree.GetDecimalValue());
+
+        Fraction fractionFour = new Fraction(1, 3);
+        Console.WriteLine(fractionFour.GetFractionString());
+        Console.WriteLine(fractionFour.GetDecimalValue());
+
     }
 }
